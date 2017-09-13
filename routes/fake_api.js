10f7962f0a48a40657ae74ebd6443d67a7fa4dcd
Nick <kiwi.sedna@gmail.com>
2017-09-13 18:06:33 +1200

    mock locations api

--- routes/fake_api.js
+++ routes/fake_api.js
@@ -1,8 +1,9 @@
+const _ = require('lodash')
 const faker = require('faker');
 const router = require('express').Router();
 
 
-const { image, helpers } = faker;
+const { image, helpers, random, lorem } = faker;
 
 const createUser = () => ({
   ...helpers.userCard(),
@@ -17,13 +18,22 @@ router.get('/current_user', (req, res) => {
 
 router.get('/user/login', (req, res) => {
   res.jsonp({
-    ok: true,
+    token: random.uuid(),
   });
 });
 
 router.get('/user/logout', (req, res) => {
   res.jsonp({
     ok: true,
+  });
+});
+
+router.get('/locations', (req, res) => {
+  res.jsonp({
+    items: _.times(10, () => ({
+      name: lorem.sentence(),
+      desc: lorem.sentences(),
+    })),
   });
 });
 
