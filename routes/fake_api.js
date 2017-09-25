680900ec736ccb4e4bc18a99fcee0f3ab7eb6674
Nick <kiwi.sedna@gmail.com>
2017-09-26 12:03:22 +1300

    generate category for locations

--- routes/fake_api.js
+++ routes/fake_api.js
@@ -1,6 +1,8 @@
-const _ = require('lodash')
+const _ = require('lodash');
 const faker = require('faker');
 const router = require('express').Router();
+
+const { CATEGORY } = require('../constants');
 
 
 const { image, helpers, random, lorem } = faker;
@@ -29,11 +31,18 @@ router.get('/user/logout', (req, res) => {
 });
 
 router.get('/locations', (req, res) => {
+  const category = CATEGORY[_.toUpper(req.query.category)];
+  const categories = _.isUndefined(category) ?
+    _.reduce(_.values(CATEGORY), (arr, category) => {
+      return arr.concat(_.values(category));
+    }, []) :
+    _.values(category);
+
   res.jsonp({
     items: _.times(10, () => ({
       id: random.uuid(),
       name: lorem.words(),
-      desc: lorem.sentence(),
+      category: _.toUpper(_.sample(categories)),
     })),
   });
 });
