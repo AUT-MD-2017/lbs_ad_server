2fcb5dfcee914eea4af71d7d6722d45de9ae7c20
Nick <kiwi.sedna@gmail.com>
2017-09-26 14:03:36 +1300

    format distance

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,9 @@
+import _ from 'lodash';
+
+
 export const formatDistance = (distance) => {
-  return distance;
+  if (distance < 1000) {
+    return `${distance}m`;
+  }
+  return `${_.round(distance / 1000, 1)}km`;
 };
