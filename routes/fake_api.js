29142d5c7341f5bc2ce18bf7c0320ad01b95ff61
Nick Qi <wei.qi.nick@gmail.com>
2017-09-26 14:04:51 +1300

    Update locations api (#2)
    * generate category for locations
    
    * generate distance
    
    * format distance

--- routes/fake_api.js
+++ routes/fake_api.js
@@ -1,6 +1,9 @@
-const _ = require('lodash')
+const _ = require('lodash');
 const faker = require('faker');
 const router = require('express').Router();
+
+const utils = require('../utils');
+const { CATEGORY, PRICE_LEVEL } = require('../constants');
 
 
 const { image, helpers, random, lorem } = faker;
@@ -29,12 +32,37 @@ router.get('/user/logout', (req, res) => {
 });
 
 router.get('/locations', (req, res) => {
+  const { query } = req;
+
+  const category = CATEGORY[_.toUpper(query.category)];
+  const categories = _.isUndefined(category) ?
+    _.reduce(_.values(CATEGORY), (arr, category) => {
+      return arr.concat(_.values(category));
+    }, []) :
+    _.values(category);
+  const priceLevels = _.values(PRICE_LEVEL);
+
+  const paginator = {
+    total: 25,
+    page: query.page || 1,
+    perPage: query.perPage || 10,
+  };
+
   res.jsonp({
-    items: _.times(10, () => ({
-      id: random.uuid(),
-      name: lorem.words(),
-      desc: lorem.sentence(),
-    })),
+    ...paginator,
+    items: _.times(paginator.perPage, (i) => {
+      const distanceBase = (paginator.page - 1 + i) * 200;
+
+      return {
+        id: random.uuid(),
+        name: lorem.words(),
+        category: _.toUpper(_.sample(categories)),
+        price: _.sample(priceLevels),
+        distance: utils.formatDistance(
+          _.random(distanceBase + 1, distanceBase + 200),
+        ),
+      };
+    }),
   });
 });
 
